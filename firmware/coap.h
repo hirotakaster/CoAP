af77ce0cd739f07d78d70b60e53dc78af495722e
niisato <hirotakaster@gmail.com>
2015-07-08 15:37:55 +0900

    update license.

--- firmware/coap.h
+++ firmware/coap.h
@@ -1,5 +1,5 @@
 /*
-MQTT library for Spark core
+CoAP library for Core/Photon.
 This software is released under the MIT License.
 Copyright (c) 2014 Hirotaka Niisato
 Permission is hereby granted, free of charge, to any person obtaining
